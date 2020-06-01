73ca4c63945a596b21642be4baff732413dfd0b2
Jon <jdog992@gmail.com>
2020-06-01 17:42:20 -0400

    drill 3

--- app.js
+++ app.js
@@ -34,7 +34,42 @@ app.get('/cipher',(req,res)=>{
 
 })
 
-
+app.get('/lotto', (req, res) => {
+    const arr = req.query.arr
+    if (arr.length != 6) {
+        return res.status(400).send('must give six numbers bewenn 1-20');
+    }
+    const arrNumbers = arr.map(number => parseInt(number))
+    const winningNumbers = [];
+    for (let i=0; i < 6; i++) {
+        function getRandomIntInclusive(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+        }
+        const randomNumber = getRandomIntInclusive(1, 20)
+        winningNumbers.push(randomNumber)
+    }
+    const correctGuesses = winningNumbers.filter(number => arrNumbers.includes(number))
+    
+    let message = '';
+    if (correctGuesses.length < 4){
+        message = 'Sorry you lose'
+    }
+    else if (correctGuesses.length = 4) {
+        message = 'Congrats you win a free ticket'
+    }
+    else if (correctGuesses.length = 5) {
+        message = 'Congrats you win $100'
+    }
+    else if (correctGuesses = 6) {
+        message = 'you won mega millions'
+    }
+    res.send(message)
+    console.log(correctGuesses)
+    console.log(arrNumbers)
+    console.log(winningNumbers)
+})
 
 app.listen(8000, () => {
     console.log('Express server is listening on port 8000!');
